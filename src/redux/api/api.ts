c14f138130d5ae8e07e54c98b831fa972b97a497
roushan-sheik <roushansheik@gmail.com>
2024-09-13 23:38:38 +0600

    add: final project

--- src/redux/api/api.ts
+++ src/redux/api/api.ts
@@ -2,6 +2,9 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const baseApi = createApi({
   reducerPath: "baseApi",
   baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:8000/api/v1" }),
+  // baseQuery: fetchBaseQuery({
+  //   baseUrl: "https://server-lyart-chi.vercel.app/api/v1",
+  // }),
   tagTypes: ["products", "verify"],
   endpoints: (builder) => ({
     getProducts: builder.query({
